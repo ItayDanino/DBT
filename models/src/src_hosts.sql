41d2d173ae03f6daae97782af2ec5e95442eb7b9
ItayDanino123 <itay.danino@vision.bi>
2025-09-08 14:04:36 +0300

    08/09

--- models/src/src_hosts.sql
+++ models/src/src_hosts.sql
@@ -1,5 +1,5 @@
 with raw_hosts as(
     -- select * from ITAY_DANINO_DBT.RAW.raw_hosts
-    {{ source('airbnb', 'hosts')}}
+    select * from {{ source('airbnb', 'hosts')}}
 )
 select * from raw_hosts
