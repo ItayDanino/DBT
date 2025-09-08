581707504c9981af3adb0b460f3242c2864ef0f3
ItayDanino123 <itay.danino@vision.bi>
2025-09-08 10:53:38 +0300

    08/09

--- models/dim/dim_listings_with_hosts.sql
+++ models/dim/dim_listings_with_hosts.sql
@@ -20,6 +20,6 @@ SELECT
     h.host_name,
     h.is_superhost as host_is_superhost,
     l.created_at,
-    GREATEST(l.updated_at, h.updated_at) as updated_at
+    GREATEST(l.created_at, h.updated_at) as updated_at
 FROM l
 LEFT JOIN h ON (h.host_id = l.host_id)
